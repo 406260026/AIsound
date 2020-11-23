15bb21462e7e01b244aef6a6993693f37d7cbdd3
406260026 <71250463+406260026@users.noreply.github.com>
2020-11-23 14:58:05 +0800

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -16,7 +16,7 @@ let classifier;
 let label = "listening";
 
 // Teachable Machine model URL:
-let soundModelURL = 'https://teachablemachine.withgoogle.com/models/h3p9R41J/model.json';
+let soundModelURL = 'https://406260026.github.io/AIsound/model.json';
 
 
 function preload() {
